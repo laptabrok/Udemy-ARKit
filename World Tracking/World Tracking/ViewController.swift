b81560ce3c67ed8973db04ccb36741c700de6443
Igor Ivanov <laptabrok@gmail.com>
2022-01-18 17:44:25 +0300

    New geometry. 

--- World Tracking/World Tracking/ViewController.swift
+++ World Tracking/World Tracking/ViewController.swift
@@ -24,10 +24,26 @@ class ViewController: UIViewController {
 
     @IBAction func addObject(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0.03)
+//        node.geometry = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0.03)
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+//        node.geometry = SCNCone(topRadius: 0.3, bottomRadius: 0.5, height: 0.3) // Cone
+//        node.geometry = SCNCone(topRadius: 0.2, bottomRadius: 0.2, height: 0.3) // Cone as cylinder
+//        node.geometry = SCNCylinder(radius: 0.2, height: 0.5)
+//        node.geometry = SCNSphere(radius: 0.5)
+//        node.geometry = SCNTube(innerRadius: 0.1, outerRadius: 0.3, height: 1.0)
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+        node.geometry = SCNPlane(width: 1.5, height: 1.0)
+        node.geometry = SCNPyramid(width: 0.5, height: 1.0, length: <#T##CGFloat#>)
+        
+        
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         node.geometry?.firstMaterial?.specular.contents = UIColor.orange
-        node.position = SCNVector3(0.0, 0.0, -0.3)
+        node.position = SCNVector3(0.0, 0.0, -1.5)
+        node.rotation = SCNVector4(x: 1.0, y: 0, z: 0, w: Float(Double.pi/2.0))
+        
+//        let transform = SCNNode()
+//        transform.rotation = SCNVector4(x: 1.0, y: 0, z: 0, w: Float(Double.pi/2.0))
+//        transform.addChildNode(node)
         
         sceneView.scene.rootNode.addChildNode(node)
     }
